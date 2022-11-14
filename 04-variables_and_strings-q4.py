fb50792b7a1961fa335b25a95f1edd3acaa1babc
Adolfo Zarrias <adolfo.zarrias@gmail.com>
2022-11-14 23:06:58 +0100

    Add section 4 questions

---
+++ 04-variables_and_strings-q4.py
@@ -0,0 +1,7 @@
+##!/usr/bin/env python
+
+# Obtain the product of two integers
+num_1 = int(input('Please enter the first number: '))
+num_2 = int(input('Please enter the second number: '))
+product = num_1 * num_2
+print('The product of', num_1, 'and', num_2, 'is', product)
